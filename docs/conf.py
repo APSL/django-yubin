644330bef147b8bffa7cb2f95bd34792b8ffa379
aaloy <aaloy@apsl.net>
2014-03-11 20:54:22 +0100

    updated title

--- docs/conf.py
+++ docs/conf.py
@@ -40,8 +40,8 @@ source_suffix = '.rst'
 master_doc = 'index'
 
 # General information about the project.
-project = u'django-mailer2'
-copyright = u'2013, Tauber, Beaven & APSL'
+project = u'django-yubin'
+copyright = u'2014, Tauber, Beaven, Disqus & APSL'
 
 # The version info for the project you're documenting, acts as replacement for
 # |version| and |release|, also used in various other places throughout the
