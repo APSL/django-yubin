7e9465bb6bf21155c9869e8c1920169f1c41061b
Sergei Maertens <sergei@maykinmedia.nl>
2015-12-24 23:46:05 +0100

    Remove unused test code

--- tests/tests/base.py
+++ tests/tests/base.py
@@ -4,9 +4,8 @@
 
 from django.core import mail
 from django.test import TestCase
+
 from django_yubin import queue_email_message
-from django.core.mail.backends.base import BaseEmailBackend
-from django.core.mail import backends
 
 
 class FakeConnection(object):
@@ -26,37 +25,18 @@ class FakeConnection(object):
         mail.outbox.append(message)
 
 
-class TestEmailBackend(BaseEmailBackend):
-    '''
-    An EmailBackend used in place of the default
-    django.core.mail.backends.smtp.EmailBackend.
-
-    '''
-    def __init__(self, fail_silently=False, **kwargs):
-        super(TestEmailBackend, self).__init__(fail_silently=fail_silently)
-        self.connection = FakeConnection()
-
-    def send_messages(self, email_messages):
-        pass
-
-
 class MailerTestCase(TestCase):
     """
     A base class for Django Mailer test cases which diverts emails to the test
     buffer and provides some helper methods.
 
     """
-    #def setUp(self):
-        #self.saved_email_backend = backends.smtp.EmailBackend
-        #backends.smtp.EmailBackend = TestEmailBackend
-
-    #def tearDown(self):
-        #backends.smtp.EmailBackend = self.saved_email_backend
 
     def queue_message(self, subject='test', message='a test message',
                       from_email='sender@djangomailer',
-                      recipient_list=['recipient@djangomailer'],
-                      priority=None):
+                      recipient_list=None, priority=None):
+        if recipient_list is None:
+            recipient_list = ['recipient@djangomailer']
         email_message = mail.EmailMessage(subject, message, from_email,
                                           recipient_list)
         return queue_email_message(email_message, priority=priority)
