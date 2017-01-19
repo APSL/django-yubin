9fb1df8a4f2c155649ef9cff5d6d62bd7dc15e72
Francesc Arpi <farpi@apsl.net>
2017-01-19 10:56:23 +0100

    Readme updated

--- setup.py
+++ setup.py
@@ -22,7 +22,7 @@ with open('docs/index.rst') as docs_index:
 
 setup(
     name='django-yubin',
-    version='0.3.1',
+    version='0.3.2',
     description=("A reusable Django app for composing and queueing emails "
                  "Adds django-mailer2 + django-mailviews + others"),
     long_description=long_description,
