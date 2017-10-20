a5e6111a39df29ed5e4a720fdc63e76eedc6787f
Daniel Sastre <dsastre@apsl.net>
2017-10-20 12:19:24 +0200

    Use a Yubin EmailMessageView in create_mail command

--- django_yubin/management/commands/create_mail.py
+++ django_yubin/management/commands/create_mail.py
@@ -1,7 +1,8 @@
 # encoding: utf-8
 
-from django.core.mail import send_mail
 from django.core.management.base import BaseCommand
+
+from ...messages import BasicHTMLEmailMessageView
 
 
 class Command(BaseCommand):
@@ -19,9 +20,11 @@ class Command(BaseCommand):
     def handle(self, *args, **options):
         number = int(options['quantity'])
         for i in range(1, number + 1):
-            send_mail(subject='test %s' % i,
-                      message='body %s' % i,
-                      from_email='test@example.com',
-                      recipient_list=['recipient%s@example.com' % i],
-                      html_message='body <strong>%s</strong>' % i)
-        self.stdout.write('Created')
+            BasicHTMLEmailMessageView(
+                subject='Subject test %s' % i,
+                content=u'Body test <strong>%s</strong> ✉️ 🙂 àäá.' % i,
+            ).send(
+                from_email='test@example.com',
+                to=['recipient%s@example.com' % i],
+            )
+        self.stdout.write('Created %d email(s).' % number)
