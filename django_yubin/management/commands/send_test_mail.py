1b87c36e18c45c20793058c951c09d517ecd0e3e
Juan Moreno <morenosan@gmail.com>
2018-06-27 11:39:08 +0200

    Add a «send_test_email» command to check connection parameters (#41)

---
+++ django_yubin/management/commands/send_test_mail.py
@@ -0,0 +1,55 @@
+# encoding: utf-8
+from __future__ import absolute_import, unicode_literals
+
+from django.conf import settings
+from django.core.management.base import BaseCommand, CommandError
+
+from ...messages import BasicHTMLEmailMessageView
+
+
+class Command(BaseCommand):
+    help = 'Send a simple email in order to check connection parameters.'
+
+    def add_arguments(self, parser):
+        parser.add_argument(
+            '-s',
+            '--subject',
+            dest='subject',
+            default='Hello World',
+            help='Email subject.',
+        )
+
+        parser.add_argument(
+            '-c',
+            '--content',
+            dest='content',
+            default='Hello World',
+            help='Email content.',
+        )
+
+        parser.add_argument(
+            '-t',
+            '--to',
+            dest='to',
+            default='',
+            help='Email address to send message.',
+        )
+
+    def handle(self, *args, **options):
+        from_email = settings.DEFAULT_FROM_EMAIL
+        to = options['to']
+        if to:
+            to_list = [to]
+        else:
+            to_list = [x[1] for x in settings.ADMINS if len(x) > 0]
+
+        if not to_list:
+            raise CommandError('Please provide an email address or set a valid settings.ADMINS configuration')
+
+        subject = options['subject']
+        content = options['content']
+        message = BasicHTMLEmailMessageView(subject, content)
+        message.send(from_email=from_email, to=to_list)
+
+        # This output is checked in tests.
+        self.stdout.write('Created email(s): 1')
--- tests/tests/test_commands.py
+++ tests/tests/test_commands.py
@@ -9,6 +9,8 @@ import time
 
 from django.core import mail
 from django.core.management import call_command
+from django.core.management.base import CommandError
+from django.conf import settings
 from django.utils.six import StringIO
 from django.utils.timezone import now
 
@@ -185,3 +187,36 @@ class TestCommands(MailerTestCase):
         call_command('create_mail', quantity=quantity, stdout=out)
         created = int(out.getvalue().split(':')[1])
         self.assertEqual(quantity, created)
+
+    def test_send_test_mail(self):
+        """
+        The ``send_test_mail`` sends a test mail to test connection params
+        """
+        out = StringIO()
+        self.assertEqual(models.Message.objects.count(), 0)
+        call_command('send_test_mail', to='test@test.com', stdout=out)
+        created = int(out.getvalue().split(':')[1])
+        self.assertEqual(1, created)
+
+    def test_send_test_no_email(self):
+        """
+        The ``send_test_mail`` without to email. Expect en error.
+        """
+        self.assertEqual(models.Message.objects.count(), 0)
+        try:
+            call_command('send_test_mail')
+            self.fail("Should fail without to address")
+        except CommandError:
+            pass
+
+    def test_send_test_no_admins(self):
+        """
+        The ``send_test_mail`` without to email. Expect en error.
+        """
+        settings.ADMINS = []
+        self.assertEqual(models.Message.objects.count(), 0)
+        try:
+            call_command('send_test_mail')
+            self.fail("Should fail without to address")
+        except CommandError:
+            pass
