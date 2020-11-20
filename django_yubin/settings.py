73c3975ba0dd8df204f1df576703e39b022cfa7c
Dani Sastre <dsastre@apsl.net>
2020-11-20 13:11:32 +0100

    Comment all settings

--- django_yubin/settings.py
+++ django_yubin/settings.py
@@ -4,12 +4,13 @@ from django.conf import settings
 # Provide a way of temporarily pausing the sending of mail.
 PAUSE_SEND = getattr(settings, "MAILER_PAUSE_SEND", False)
 
-USE_BACKEND = getattr(settings, 'MAILER_USE_BACKEND',
-                      'django.core.mail.backends.smtp.EmailBackend')
+# Real backend to send emails.
+USE_BACKEND = getattr(settings, 'MAILER_USE_BACKEND', 'django.core.mail.backends.smtp.EmailBackend')
 
 # When MAILER_TEST_MODE is True, recipient addresses of all messages are replaced with
 # the email addresses set in MAILER_TEST_EMAIL before being sent
 MAILER_TEST_MODE = getattr(settings, "MAILER_TEST_MODE", False)
 MAILER_TEST_EMAIL = getattr(settings, "MAILER_TEST_EMAIL", '')
 
+# If there are emails to be enqueued or sent for more than x minutes, HealthCheck will mark an error
 MAILER_HC_QUEUED_LIMIT_OLD = getattr(settings, "MAILER_HC_QUEUED_LIMIT_OLD", 30)
