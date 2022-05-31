aa18758d0138023e886ad6ff44d60aef320f3b6b
Dani Sastre <dsastre@apsl.net>
2022-05-31 18:17:04 +0200

    Remove unsupported Python and Django versions

--- setup.py
+++ setup.py
@@ -44,14 +44,11 @@ setup(
         'License :: OSI Approved :: Apache Software License',
         'Operating System :: OS Independent',
         'Programming Language :: Python',
-        'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
         'Programming Language :: Python :: 3.8',
         'Programming Language :: Python :: 3.9',
+        'Programming Language :: Python :: 3.10',
         'Framework :: Django',
-        'Framework :: Django :: 2.2',
-        'Framework :: Django :: 3.0',
-        'Framework :: Django :: 3.1',
         'Framework :: Django :: 3.2',
     ]
 )
