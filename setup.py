7a13a57c2a7f9413a37186ba06827ddcb1c5e1d0
Dani Sastre <dsastre@apsl.net>
2023-01-24 10:16:21 +0100

    support for Django 4.1 in tests and github actions

--- setup.py
+++ setup.py
@@ -47,12 +47,13 @@ setup(
         'License :: OSI Approved :: Apache Software License',
         'Operating System :: OS Independent',
         'Programming Language :: Python',
-        'Programming Language :: Python :: 3.7',
         'Programming Language :: Python :: 3.8',
         'Programming Language :: Python :: 3.9',
         'Programming Language :: Python :: 3.10',
+        'Programming Language :: Python :: 3.11',
         'Framework :: Django',
         'Framework :: Django :: 3.2',
+        'Framework :: Django :: 4.0',
         'Framework :: Django :: 4.1',
     ]
 )
