90ed636b1b1c7d3028c428e9e48a85683e619903
Viicos <65306057+Viicos@users.noreply.github.com>
2023-11-08 11:30:38 +0100

    Call Celery task with `transaction.on_commit`

--- django_yubin/models.py
+++ django_yubin/models.py
@@ -4,6 +4,7 @@ import email
 from email import policy
 from email import encoders as Encoders
 from email.mime.base import MIMEBase
+from functools import partial
 
 from django.core.exceptions import FieldError
 from django.core.mail.message import (
@@ -11,7 +12,7 @@ from django.core.mail.message import (
         EmailMessage,
         EmailMultiAlternatives,
     )
-from django.db import models
+from django.db import models, transaction
 from django.db.models import F
 from django.utils.module_loading import import_string
 from django.utils.text import Truncator
@@ -254,7 +255,7 @@ class Message(models.Model):
         self.mark_as(self.STATUS_QUEUED, log_message)
 
         try:
-            tasks.send_email.delay(self.pk)
+            transaction.on_commit(partial(tasks.send_email.delay, message_pk=self.pk))
             return True
         except Exception as e:
             self.date_enqueued = backup['date_enqueued']
