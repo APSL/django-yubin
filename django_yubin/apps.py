e63e9bac95a599e039103e4ec4136a51cdca2f67
Dani Sastre <dsastre@apsl.net>
2024-06-21 11:18:35 +0200

    Use `BigAutoField` and migrate all existing auto fields to it

--- django_yubin/apps.py
+++ django_yubin/apps.py
@@ -3,7 +3,8 @@ from django.apps import AppConfig
 
 class YubinConfig(AppConfig):
     name = 'django_yubin'
-    verbose_name = "Django Yubin"
+    verbose_name = 'Django Yubin'
+    default_auto_field = 'django.db.models.BigAutoField'
 
     def ready(self):
         from . import signals  # noqa
